0573a41963598a4855441a25442e83bc9b50371d
Utkarsh Yadav <131299458+UtkarshYadav360@users.noreply.github.com>
2023-09-09 12:04:48 +0530

    Delete Day 16 directory
    I want to change the folder name.

--- Day 16/making_my_own_module.py
+++
@@ -1,1 +0,0 @@
-text = "This module is created by myself."
--- Day 16/using my_own_made_module.py
+++
@@ -1,7 +0,0 @@
-# using a self made module
-
-# importing the module
-import making_my_own_module
-
-# using the module
-print(making_my_own_module.text)
